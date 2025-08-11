4037a44d1dffc52717bef4851ffc92ed0ed7cddc
demonlord2002 <drdoom2003p@gmail.com>
2025-08-12 00:17:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # === Telegram API ===
-API_ID = int(os.getenv("API_ID", "0"))  # Your Telegram API ID (integer)
-API_HASH = os.getenv("API_HASH", "")    # Your Telegram API Hash
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")  # Your bot token from BotFather
+API_ID = int(os.getenv("API_ID", "23559126"))  # Your Telegram API ID (integer)
+API_HASH = os.getenv("API_HASH", "58347a441c011b1b9ee3367ea936dcc4")    # Your Telegram API Hash
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8289940670:AAGQ8Z49PV6wZ_sfY5JWl192wPWPWyeBDCk")  # Your bot token from BotFather
 
 # === MongoDB ===
-MONGO_URI = os.getenv("MONGO_URI", "")  # MongoDB connection URI
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://drdoom2003p:drdoom2003p@cluster0.fnhjrtn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB connection URI
 
 # === Crunchyroll Credentials (optional) ===
-CRUNCHYROLL_USER = os.getenv("CRUNCHYROLL_USER", "")
-CRUNCHYROLL_PASS = os.getenv("CRUNCHYROLL_PASS", "")
+CRUNCHYROLL_USER = os.getenv("CRUNCHYROLL_USER", "jg_717@hotmail.com")
+CRUNCHYROLL_PASS = os.getenv("CRUNCHYROLL_PASS", "J7173659207g.")
 
 # === yt-dlp settings ===
 YTDLP_PATH = os.getenv("YTDLP_PATH", "yt-dlp")  # yt-dlp binary name/path
